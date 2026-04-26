64f296372b503c9b88429a7dee49552c120af820
Lisa <lalisssa.monobal@gmail.com>
2026-04-26 21:18:22 +0300

    initial: setup dev dash project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Notes from './components/Notes.jsx';
+
+function App () {
+    return(
+        <div className='main-content'>
+            <Notes/>
+        </div>
+    )
+};
+
+export default App;
---
+++ src/components/Notes.jsx
@@ -0,0 +1,10 @@
+import '../App.css'; 
+import '../styles/Notes.css'
+
+export default function Notes () {
+    return(
+        <div className='notes-container'>
+            Here will be notes soon!
+        </div>
+    )
+}
