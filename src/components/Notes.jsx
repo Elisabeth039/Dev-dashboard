0a76a771bdbf2124c53d3880f77be31bdf252564
Lisa <lalisssa.monobal@gmail.com>
2026-04-29 22:28:04 +0300

    feat: add folders UI and basic state logic

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,10 +1,77 @@
+import { useState } from 'react';
 import '../App.css'; 
-import '../styles/Notes.css'
+import '../styles/Notes.css';
 
 export default function Notes () {
+    const [folders, setFolders] = useState([
+        {id: 1, name: 'All notes'},
+        {id: 2, name: 'Another folder'}
+    ]);
+
+    const [activeFolder, setActiveFolder] = useState("All notes");
+    const [input, setInput] = useState('');
+    const [isOpen, setIsOpen] = useState(false);
+
+    const addFolder = () => {
+        if (input.trim() === '') return;
+
+        const newFolder = {
+            id: Date.now(),
+            name: input
+        };
+
+        setFolders(prev => [...prev, newFolder]);
+        setInput('');
+        setIsOpen(false);
+    }; 
+
     return(
         <div className='notes-container'>
-            Here will be notes soon!
+            <input
+                type='text'
+                placeholder=' ⌕ Search notes...'
+                className='search'
+            />
+
+            <div className='folders'>
+                <button 
+                    onClick={() => setIsOpen(true)}
+                    className='pill'
+                >
+                    🗀
+                </button>
+
+                {folders.map(folder =>(
+                    <button
+                        key={folder.id}
+                        className={`pill ${
+                            activeFolder === folder.name ? "active" : ""
+                        }`}
+                        onClick={() => setActiveFolder(folder.name)}
+                    >
+                        {folder.name}
+                    </button>
+                ))}
+            </div>
+
+            <div className='notes-section'></div>
+
+            {isOpen && (
+                <div className='add-folder-panel'>
+                    <input
+                        type='text'
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                        className='add-input'
+                    />
+                    <button
+                        className='add-folder'
+                        onClick={addFolder}
+                    >
+                        Add folder
+                    </button>
+                </div>
+            )}
         </div>
-    )
+    );
 }
